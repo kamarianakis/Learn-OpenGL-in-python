d9d988765d8627aa7700d690d262683cd7a47669
Manos Kamarianakis <m.kamarianakis@gmail.com>
2023-12-01 20:28:09 +0200

    Fix example 16

--- ep16_camera.py
+++ ep16_camera.py
@@ -42,7 +42,7 @@ def mouse_enter_clb(window, entered):
 
 
 vertex_src = """
-# version 330
+# version 410
 
 layout(location = 0) in vec3 a_position;
 layout(location = 1) in vec2 a_texture;
@@ -62,7 +62,7 @@ void main()
 """
 
 fragment_src = """
-# version 330
+# version 410
 
 in vec2 v_texture;
 
@@ -89,6 +89,11 @@ if not glfw.init():
     raise Exception("glfw can not be initialized!")
 
 # creating the window
+glfw.window_hint(glfw.CONTEXT_VERSION_MAJOR, 4)
+glfw.window_hint(glfw.CONTEXT_VERSION_MINOR, 1)
+glfw.window_hint(glfw.OPENGL_FORWARD_COMPAT, GL_TRUE)
+glfw.window_hint(glfw.OPENGL_PROFILE, glfw.OPENGL_CORE_PROFILE)
+
 window = glfw.create_window(WIDTH, HEIGHT, "My OpenGL window", None, None)
 
 # check if window was created
@@ -108,7 +113,8 @@ glfw.set_cursor_enter_callback(window, mouse_enter_clb)
 
 # make the context current
 glfw.make_context_current(window)
-
+VAO = glGenVertexArrays(1)
+glBindVertexArray(VAO)
 # load here the 3d meshes
 cube_indices, cube_buffer = ObjLoader.load_model("meshes/cube.obj")
 monkey_indices, monkey_buffer = ObjLoader.load_model("meshes/monkey.obj")
